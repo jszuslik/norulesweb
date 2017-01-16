ef17b309754b4f6bb68b238750d2f9a7f554948a
Joshua Szuslik <josh@lanex.com>
2017-01-16 17:07:43 -0600

    Update

--- core/functions/seo/js/nrw-structured-data.js
+++ core/functions/seo/js/nrw-structured-data.js
@@ -5,14 +5,18 @@ jQuery(document).ready(function($) {
         .done(function(data) {
             var data_table = $('.form-table').eq(3);
             var data_table_body = data_table.find('tbody');
-            var scope_string = '<tr><th>Scope</th><td><select name="scopelist">';
+            var scope_string = '<tr><th>Scope</th><td><select id="scopelist" name="scopelist">';
             console.log(data.children);
             $.each(data.children, function(key, object) {
-                scope_string += '<option>' + object.name + '</option>';
+                scope_string += '<option value="'+ key + '">' + object.name + '</option>';
             });
             scope_string += '</select></td></tr>';
             console.log(scope_string);
             data_table_body.append(scope_string);
+            $('#scopelist').change(function() {
+                var x = $(this).val();
+                $('#add_structured_data_form').val(x);
+            })
         });
 
 
