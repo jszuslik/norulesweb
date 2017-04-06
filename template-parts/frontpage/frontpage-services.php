4e8a77ae97c4c96e26784137aef5712ad6cf6954
Joshua Szuslik <josh@lanex.com>
2017-04-06 13:02:29 -0500

    Update

--- template-parts/frontpage/frontpage-services.php
+++ template-parts/frontpage/frontpage-services.php
@@ -1,16 +1,30 @@
 <?php ?>
 <div id="js__scroll-to-section" class="container g-padding-y-80--xs g-padding-y-50--sm">
     <div class="g-text-center--xs">
-        <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2 g-margin-b-25--xs">Web Services</p>
+        <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2 g-margin-b-25--xs">Welcome to No Rules Web</p>
         <h2 class="g-font-size-32--xs g-font-size-26--md">We work hard to ensure that every customer is a success story. With each project we look for the areas that’ll have the biggest impact so you can get the greatest return. Learn more about what we provide below.</h2>
     </div>
 </div>
+
+<!-- Parallax -->
+<div class="js__parallax-window" style="background: url('http://norulesweb.dev/wp-content/uploads/2017/04/milwaukee_bw_1920x1080.jpg') 50% 0 no-repeat fixed;">
+    <div class="overlay-grid" style="position: relative; top: 0; width: 100%; height: 100%; background: rgba(3, 77, 161, 0.5);">
+        <div class="container g-text-center--xs g-padding-y-80--xs g-padding-y-125--sm">
+            <div class="g-margin-b-80--xs">
+                <h2 class="g-font-size-40--xs g-font-size-50--sm g-font-size-60--md g-color--white">The Fastest Way To Develop</h2>
+            </div>
+            <a href="http://keenthemes.com/" class="text-uppercase s-btn s-btn--md s-btn--white-brd g-radius--50">Learn More</a>
+        </div>
+    </div>
+</div>
+<!-- End Parallax -->
+
 <!-- Culture -->
 <div class="g-promo-section">
-    <div class="container g-padding-y-80--xs g-padding-y-20--sm">
+    <div class="container g-padding-y-80--xs g-padding-y-125--sm">
         <div class="row">
             <div class="col-md-4 g-margin-t-15--xs g-margin-b-60--xs g-margin-b-0--lg">
-<!--                <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2 g-margin-b-25--xs">Culture</p>-->
+                <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2 g-margin-b-25--xs">Web Services</p>
                 <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".1s">
                     <h2 class="g-font-size-40--xs g-font-size-50--sm g-font-size-60--md">Web Design</h2>
                 </div>
@@ -24,9 +38,78 @@
             </div>
         </div>
     </div>
-    <div class="col-sm-3 g-promo-section__img-right--lg g-bg-position--center g-height-100-percent--md js__fullwidth-img">
-        <img class="img-responsive" src="img/970x970/03.jpg" alt="Image">
+    <div class="col-sm-3 g-promo-section__img-right--lg g-bg-position--center g-height-100-percent--md" style="background: url('http://norulesweb.dev/wp-content/uploads/2017/04/web_design_ux_bw_970x970.jpg') no-repeat; padding: 0; opacity: 0.9">
+        <div class="overlay-grid" style="position: relative; top: 0; width: 100%; height: 100%; background: url('http://norulesweb.dev/wp-content/uploads/2017/04/bright-squares.png')"></div>
+<!--        <img class="img-responsive" src="wp-content/uploads/2017/04/web_design_ux_970x970.jpg" alt="Image">-->
+    </div>
+</div>
+<div class="g-promo-section">
+    <div class="container g-padding-y-80--xs g-padding-y-125--sm">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-3">
+                <p class="g-font-size-18--xs">We aim high at being focused on building relationships with our clients and community. Using our creative gifts drives this foundation. The time has come to bring those ideas and plans to life. This is where we really begin to visualize your napkin sketches and make them into beautiful pixels.</p>
+                <p class="g-font-size-18--xs">Now that your brand is all dressed up and ready to party, it's time to release it to the world. By the way, let's celebrate already.</p>
+            </div>
+            <div class="col-md-4 g-margin-t-15--xs g-margin-b-60--xs g-margin-b-0--lg col-md-offset-1">
+                <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2 g-margin-b-25--xs">Web Services</p>
+                <div class="wow fadeInRight" data-wow-duration=".3" data-wow-delay=".1s">
+                    <h2 class="g-font-size-40--xs g-font-size-50--sm g-font-size-60--md">Web</h2>
+                </div>
+                <div class="wow fadeInRight" data-wow-duration=".3" data-wow-delay=".3s">
+                    <h2 class="g-font-size-40--xs g-font-size-50--sm g-font-size-60--md">Development</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3 g-promo-section__img-left--lg g-bg-position--center g-height-100-percent--md" style="background: url('http://norulesweb.dev/wp-content/uploads/2017/04/code_bw_970x970.jpg') no-repeat; padding: 0; opacity: 0.9">
+        <div class="overlay-grid" style="position: relative; top: 0; width: 100%; height: 100%; background: url('http://norulesweb.dev/wp-content/uploads/2017/04/bright-squares.png')"></div>
+        <!--        <img class="img-responsive" src="wp-content/uploads/2017/04/web_design_ux_970x970.jpg" alt="Image">-->
+    </div>
+</div>
+<div class="g-promo-section">
+    <div class="container g-padding-y-80--xs g-padding-y-125--sm">
+        <div class="row">
+            <div class="col-md-4 g-margin-t-15--xs g-margin-b-60--xs g-margin-b-0--lg">
+                <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2 g-margin-b-25--xs">Web Services</p>
+                <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".1s">
+                    <h2 class="g-font-size-40--xs g-font-size-50--sm g-font-size-60--md">Analytics &</h2>
+                </div>
+                <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".3s">
+                    <h2 class="g-font-size-40--xs g-font-size-50--sm g-font-size-60--md">Optimization</h2>
+                </div>
+            </div>
+            <div class="col-md-4 col-md-offset-1">
+                <p class="g-font-size-18--xs">We aim high at being focused on building relationships with our clients and community. Using our creative gifts drives this foundation. The time has come to bring those ideas and plans to life. This is where we really begin to visualize your napkin sketches and make them into beautiful pixels.</p>
+                <p class="g-font-size-18--xs">Now that your brand is all dressed up and ready to party, it's time to release it to the world. By the way, let's celebrate already.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3 g-promo-section__img-right--lg g-bg-position--center g-height-100-percent--md" style="background: url('http://norulesweb.dev/wp-content/uploads/2017/04/analytics_bw_970x970.jpg') no-repeat; padding: 0; opacity: 0.9">
+        <div class="overlay-grid" style="position: relative; top: 0; width: 100%; height: 100%; background: url('http://norulesweb.dev/wp-content/uploads/2017/04/bright-squares.png');"></div>
+        <!--        <img class="img-responsive" src="wp-content/uploads/2017/04/web_design_ux_970x970.jpg" alt="Image">-->
     </div>
 </div>
 <!-- End Culture -->
+<!-- Subscribe -->
+<div class="js__parallax-window" style="background: url('http://norulesweb.dev/wp-content/uploads/2017/04/milwaukee_bw_1920x1080.jpg') 50% 0 no-repeat fixed;">
+    <div class="overlay-grid" style="position: relative; top: 0; width: 100%; height: 100%; background: rgba(3, 77, 161, 0.5);">
+        <div class="g-container--sm g-text-center--xs g-padding-y-80--xs g-padding-y-125--sm">
+            <div class="g-margin-b-80--xs">
+                <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--white-opacity g-letter-spacing--2 g-margin-b-25--xs">Subscribe</p>
+                <h2 class="g-font-size-32--xs g-font-size-36--md g-color--white">Join Over 1000+ People</h2>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+                    <form class="input-group">
+                        <input type="email" class="form-control s-form-v1__input g-radius--left-50" name="email" placeholder="Enter your email">
+                        <span class="input-group-btn">
+                                    <button type="submit" class="s-btn s-btn-icon--md s-btn-icon--white-brd s-btn--white-brd g-radius--right-50"><i class="ti-arrow-right"></i></button>
+                                </span>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Subscribe -->
 
