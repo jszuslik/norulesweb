b407f5bd90863796db6ee54bbea0097a22385b8e
Joshua Szuslik <josh@lanex.com>
2017-04-24 17:06:06 -0500

    Update to dynamic meta fields

--- admin/page/meta/meta-boxes.php
+++ admin/page/meta/meta-boxes.php
@@ -13,6 +13,48 @@ class NrwPageMeta {
             add_action('save_post', array( $this, 'nrw_save_page_meta' ) );
             add_action('admin_print_styles', array( $this, 'nrw_meta_image_enqueue'));
         }
+        $this->hp_meta_groups = array(
+	        array(
+		        'name' => __('Funnel Images', NRW_TEXT_DOMAIN),
+		        'fields' => array(
+			        array(
+				        'type' => 'image',
+				        'name' => 'nrw_meta_image_1',
+				        'id' => 'nrw_meta_image_1',
+				        'btn_id' => 'nrw_image_btn_1',
+				        'label' => __('Image 1', NRW_TEXT_DOMAIN),
+				        'description' => 'This image displays on the left of the three image grid'
+			        ),
+			        array(
+				        'type' => 'image',
+				        'name' => 'nrw_meta_image_2',
+				        'id' => 'nrw_meta_image_2',
+				        'btn_id' => 'nrw_image_btn_2',
+				        'label' => __('Image 2', NRW_TEXT_DOMAIN),
+				        'description' => 'This image displays on the top right of the three image grid'
+			        ),array(
+				        'type' => 'image',
+				        'name' => 'nrw_meta_image_3',
+				        'id' => 'nrw_meta_image_3',
+				        'btn_id' => 'nrw_image_btn_3',
+				        'label' => __('Image 3', NRW_TEXT_DOMAIN),
+				        'description' => 'This image displays on the bottom right of the three image grid'
+			        )
+		        )
+	        ),
+	        array(
+		        'name' => __('Funnel Heading', NRW_TEXT_DOMAIN),
+		        'fields' => array(
+			        array(
+				        'type' => 'text',
+				        'name' => 'nrw_page_heading',
+				        'id' => 'nrw_page_heading',
+				        'label' => __('Funnel Heading', NRW_TEXT_DOMAIN),
+				        'description' => ''
+			        )
+		        )
+	        )
+        );
     }
 
     public function nrw_home_page_meta() {
@@ -28,39 +70,16 @@ class NrwPageMeta {
             add_meta_box('nrw_page_meta', __('Funnel Settings', NRW_TEXT_DOMAIN), array($this, 'nrw_page_meta_callback'), array('page'));
         }
     }
+    
+    private $prefix = 'nrw_';
+    
+    private $hp_meta_groups = '';
 
     public function nrw_home_page_meta_callback( $post ) {
         wp_nonce_field(basename(__FILE__), NRW_PAGE_NONCE);
         $nrw_stored_page_meta = get_post_meta($post->ID);
-        $field_array = array(
-            array(
-                'type' => 'image',
-                'name' => 'nrw_meta_image_1',
-                'id' => 'nrw_meta_image_1',
-                'btn_id' => 'nrw_image_btn_1',
-                'meta_id' => $nrw_stored_page_meta,
-                'label' => __('Image 1', NRW_TEXT_DOMAIN),
-                'description' => 'This image displays on the left of the three image grid'
-            ),
-            array(
-                'type' => 'image',
-                'name' => 'nrw_meta_image_2',
-                'id' => 'nrw_meta_image_2',
-                'btn_id' => 'nrw_image_btn_2',
-                'meta_id' => $nrw_stored_page_meta,
-                'label' => __('Image 2', NRW_TEXT_DOMAIN),
-                'description' => 'This image displays on the top right of the three image grid'
-            ),array(
-                'type' => 'image',
-                'name' => 'nrw_meta_image_3',
-                'id' => 'nrw_meta_image_3',
-                'btn_id' => 'nrw_image_btn_3',
-                'meta_id' => $nrw_stored_page_meta,
-                'label' => __('Image 3', NRW_TEXT_DOMAIN),
-                'description' => 'This image displays on the bottom right of the three image grid'
-            )
-        );
-        $this->do_meta_fields($field_array);
+        
+        $this->do_meta_fields($this->hp_meta_groups, $nrw_stored_page_meta);
     }
 
     public function nrw_page_meta_callback( $post ) {
@@ -84,45 +103,48 @@ class NrwPageMeta {
                 'description' => 'This image displays on the parents page'
             )
         );
-        $this->do_meta_fields($field_array);
+        $this->do_meta_fields($field_array, $post->ID);
     }
 
-    public function do_meta_fields($field_array) {
+    public function do_meta_fields($field_array, $nrw_stored_page_meta) {
         $fields = '';
-        foreach($field_array as $field) {
-            $meta_fields = $field['meta_id'];
-            $value = null;
-            $type = $field['type'];
-            $name = $field['name'];
-            $id = $field['id'];
-            $label = $field['label'];
-            $btn_id = null;
-            if(isset($field['btn_id']))
-                $btn_id = $field['btn_id'];
-            if(isset($meta_fields[$name]))
-                $value = $meta_fields[$name];
-            $description = $field['description'];
-            switch($type){
-                case 'text':
-                    $fields .= '<div>';
-                    $fields .= '<label>' . $label . '</label>';
-                    $fields .= '<p><input type="' . $type . '" name="' . $name . '" id="' . $id . '" value="' . $value[0] . '" style="width: 100%" /></p>';
-                    $fields .= '</div>';
-                    break;
-                case 'textarea':
-                    $fields .= '<div>';
-                    $fields .= '<label>' . $label . '</br><small>' . $description . '</small></label>';
-                    $fields .= '<p><textarea name="' . $name . '" id="' . $id . '" rows="4" style="width:100%">' . $value[0] . '</textarea></p>';
-                    $fields .= '</div>';
-                    break;
-                case 'image':
-                    $fields .= '<div>';
-                    $fields .= '<label>' . $label . '</label><br><small>' . $description . '</small>';
-                    $fields .= '<><input type="text" name="' . $name . '" id="' . $id . '" value="' . $value[0] . '" style="width: 100%" /><br>';
-                    $fields .= '<input type="button" id="' . $btn_id . '" class="button nrw_button" value="'. __( 'Choose or Upload an Image', NRW_TEXT_DOMAIN) .'"/></p>';
-                    $fields .= '</div>';
-            }
+        foreach($field_array as $field_group) {
+	        $fields .= '<h2>' . $field_group['name'] . '</h2>';
+	        foreach($field_group['fields'] as $field) {
+		        $value = null;
+		        $type = $field['type'];
+		        $name = $field['name'];
+		        $id = $field['id'];
+		        $label = $field['label'];
+		        $btn_id = null;
+		        if(isset($field['btn_id']))
+			        $btn_id = $field['btn_id'];
+		        $value = $nrw_stored_page_meta[$id];
+		        // p($value);
+		        $description = $field['description'];
+		        switch($type){
+			        case 'text':
+				        $fields .= '<div>';
+				        $fields .= '<label>' . $label . '</label>';
+				        $fields .= '<p><input type="' . $type . '" name="' . $name . '" id="' . $id . '" value="' . $value[0] . '" style="width: 100%" /></p>';
+				        $fields .= '</div>';
+				        break;
+			        case 'textarea':
+				        $fields .= '<div>';
+				        $fields .= '<label>' . $label . '</br><small>' . $description . '</small></label>';
+				        $fields .= '<p><textarea name="' . $name . '" id="' . $id . '" rows="4" style="width:100%">' . $value[0] . '</textarea></p>';
+				        $fields .= '</div>';
+				        break;
+			        case 'image':
+				        $fields .= '<div>';
+				        $fields .= '<label>' . $label . '</label><br><small>' . $description . '</small>';
+				        $fields .= '<input type="text" name="' . $name . '" id="' . $id . '" value="' . $value[0] . '" style="width: 100%" /><br>';
+				        $fields .= '<input type="button" id="' . $btn_id . '" class="button nrw_button" value="'. __( 'Choose or Upload an Image', NRW_TEXT_DOMAIN) .'"/></p>';
+				        $fields .= '</div>';
+		        }
+	        }
         }
+        
         echo $fields;
     }
 
@@ -134,20 +156,19 @@ class NrwPageMeta {
         if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
             return;
         }
+	    foreach($this->hp_meta_groups as $field_group) {
+		    foreach($field_group['fields'] as $field) {
+			    if (isset($_POST[$field['id']])) {
+				    update_post_meta( $post_id, $field['id'], sanitize_text_field($_POST[$field['id']] ) );
+			    }
+		    }
+	    }
+        
         if (isset($_POST['nrw_page_btn'])) {
             update_post_meta( $post_id, 'nrw_page_btn', sanitize_text_field($_POST['nrw_page_btn'] ) );
         }
         if (isset($_POST['nrw_meta_image'])) {
             update_post_meta( $post_id, 'nrw_meta_image', sanitize_text_field($_POST['nrw_meta_image'] ) );
-        }
-        if (isset($_POST['nrw_meta_image_1'])) {
-            update_post_meta( $post_id, 'nrw_meta_image_1', sanitize_text_field($_POST['nrw_meta_image_1'] ) );
-        }
-        if (isset($_POST['nrw_meta_image_2'])) {
-            update_post_meta( $post_id, 'nrw_meta_image_2', sanitize_text_field($_POST['nrw_meta_image_2'] ) );
-        }
-        if (isset($_POST['nrw_meta_image_3'])) {
-            update_post_meta( $post_id, 'nrw_meta_image_3', sanitize_text_field($_POST['nrw_meta_image_3'] ) );
         }
     }
 
